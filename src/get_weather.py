69971e90f438cd077cb7c4a2f3a93883dba46cf9
bambou932OS <bambou932+OS@gmail.com>
2023-06-09 12:54:23 +0900

    Add HTTPError Handling

--- src/get_weather.py
+++ src/get_weather.py
@@ -18,10 +18,33 @@ def build_url(location, metric=True):
     
     return url
 
+
 def get_weather(location, metric=True):
     query_url = build_url(location, metric)
 
-    with request.urlopen(query_url) as response: # Get the response from the URL
-        data = response.read()
+    try:
+        with request.urlopen(query_url) as response: # Get the response from the URL
+            data = response.read()
+    except error.HTTPError as e:
+        if(e.code == 400):
+            print('400 Bad Request.')
+            print('If this error occurs periodically, please report an issue')
+
+        if(e.code == 401):
+            print('Invalid API Key. Enter Valid API Key.')
+            api_key_parser.make_api_key_file()
+        
+        if(e.code == 404):
+            print('City not found. Please enter valid city name.')
+            raise SystemExit
+        
+        if(e.code == 429):
+            print('Too many requests. Please try again later.')
+            raise SystemExit
+        
+        if(e.code//100 == 5): # 5xx Server Error
+            print('Unexpected Error. Check Internet connection and try again later.')
+            raise SystemExit
+
 
     return json.loads(data) # Convert the response to a Python dict
